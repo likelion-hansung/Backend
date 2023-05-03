b368a9b9f05133783d8c0d2bcabca79c423b056e
ljy6712 <a01045386712@gmail.com>
2023-05-04 05:14:02 +0900

    5/4 이지연 #14

---
+++ [2주차]/jiyeon/mystite/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+class AboutView(TemplateView):
+    template_name = 'about.html'
+class ServicesView(TemplateView):
+    template_name = 'services.html'
+class PortfolioView(TemplateView):
+    template_name = 'portfolio.html'
+class ContactView(TemplateView):
+    template_name = 'contact.html'
